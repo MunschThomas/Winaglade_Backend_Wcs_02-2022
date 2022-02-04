5b7b894f90daadc4408d381120a69db69afad446
Pinchon Léo <leopinchonspa@gmail.com>
2022-02-04 12:39:57 +0100

    ccote

--- routes/cotes.js
+++ routes/cotes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require("../helper/db.js");
 
 router.get("/", (req, res) => {
-let sql = "SELECT name_glad, name_epreuve, cote FROM glad.glad AS g JOIN glad_epreuve AS ge ON ge.glad_id = g.id JOIN epreuve AS e ON ge.epreuve_id = e.id;"
+let sql = "SELECT g.id, name_glad, name_epreuve, cote FROM glad.glad AS g JOIN glad_epreuve AS ge ON ge.glad_id = g.id JOIN epreuve AS e ON ge.epreuve_id = e.id;"
 
   connection.query(sql, (err, result) => {
     if (err) {
