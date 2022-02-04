f20d0b6ab728f83ff3f23f6e53829df766011f40
Pinchon Léo <leopinchonspa@gmail.com>
2022-02-04 01:48:51 +0100

    ajout image sans

--- routes/glad.js
+++ routes/glad.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require("../helper/db.js");
 
 router.get("/", (req, res) => {
-let sql = "SELECT g.name_glad, g.image, g.age, g.taille, g.VIT, g.ACC, g.MAN, g.ATT, g.DEF, g.AGI, a.name_arme, m.name_metier, v.name_ville, t.name_team FROM glad AS g JOIN arme AS a ON g.arme_id = a.id JOIN metier AS m ON g.metier_id = m.id JOIN ville AS v ON g.ville_id = v.id JOIN team AS t ON g.team_id = t.id;"
+let sql = "SELECT g.name_glad, g.image, g.imageSans, g.age, g.taille, g.VIT, g.ACC, g.MAN, g.ATT, g.DEF, g.AGI, a.name_arme, m.name_metier, v.name_ville, t.name_team FROM glad AS g JOIN arme AS a ON g.arme_id = a.id JOIN metier AS m ON g.metier_id = m.id JOIN ville AS v ON g.ville_id = v.id JOIN team AS t ON g.team_id = t.id;"
 //   let sql = "SELECT * FROM arme JOIN glad ON glad.arme_id = arme.id";
 //   let sql= "SELECT * FROM glad "
 
